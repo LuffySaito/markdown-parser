9ee1c2cbe9bad88666e9194495be63cb05dbd433
AllKeng <>
2022-04-21 13:27:47 -0700

    JUnit testsGetLink for test-file.md
    modified test-file.md to be correct with report. but command is:
    java -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.*;
 /**
  * This file is intended to test the functionalities of MarkDownParse.
  */
@@ -7,5 +10,14 @@ public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testGetLinks() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("https://something.com",
+        "some-page.html");
+        assertEquals(expected,MarkdownParse.getLinks(content));
     }
 }
