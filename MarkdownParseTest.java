e6eac68bff63122696c2625f39956625e610c59a
AllKeng <>
2022-04-21 13:35:15 -0700

    MarkdownParse for all 4 testfiles

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,11 +13,38 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testGetLinks() throws IOException {
+    public void testGetLinksForTestFile1() throws IOException {
         Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
         List<String> expected = List.of("https://something.com",
         "some-page.html");
         assertEquals(expected,MarkdownParse.getLinks(content));
     }
+
+    @Test
+    public void testGetLinksForTestFile2() throws IOException {
+        Path fileName = Path.of("testfile2.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("www.myspace.com",
+        "www.facebook.com");
+        assertEquals(expected,MarkdownParse.getLinks(content));
+    }
+    @Test
+    public void testGetLinksForTestFile3() throws IOException {
+        Path fileName = Path.of("testfile3.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("www.speedrunners.com",
+        "www.google.com");
+        assertEquals(expected,MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testGetLinksForTestFile4() throws IOException {
+        Path fileName = Path.of("try-break.md");
+        String content = Files.readString(fileName);
+        List<String> expected = List.of("https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "image.png");
+        assertEquals(expected,MarkdownParse.getLinks(content));
+    }
+    
 }
